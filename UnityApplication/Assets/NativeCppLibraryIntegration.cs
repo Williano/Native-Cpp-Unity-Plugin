a7dfe9f3020896dd10da9db48d857226716cc50c
Williano <paawilly17@gmail.com>
2020-01-17 16:22:02 -0600

    Import and run native C++ functions in unity script

---
+++ UnityApplication/Assets/NativeCppLibraryIntegration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class NativeCppLibraryIntegration : MonoBehaviour
+{
+
+    // Import and expose native c++ functions
+
+    [DllImport("NATIVECPPLIBRARY", EntryPoint = "displayNumber")]
+    public static extern int displayNumber();
+
+    [DllImport("NATIVECPPLIBRARY", EntryPoint = "getRandom")]
+    public static extern int getRandom();
+
+    [DllImport("NATIVECPPLIBRARY", EntryPoint = "displaySum")]
+    public static extern int displaySum();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Display output of functions
+        print(displayNumber());
+
+        print(getRandom());
+
+        print(displaySum());
+        
+    }
+
+
+}
